8f1e4f5f9cd23b4e81a7b6a0c287a7beec322234
Hardeep567 <hardeepnaresh8@gmail.com>
2024-09-15 10:16:46 -0700

    Create Task 3.c

---
+++ LAB 4/Task 3.c
@@ -0,0 +1,18 @@
+/*
+ *NAME : HARDEEP NARESH
+ *Date : 9 Sept 2024
+ *Desc : The number is multiple of 3
+ */
+#include<stdio.h>
+int main(){
+ int a;
+ printf("Enter the number");
+ scanf("%d", &a);
+if(a%3==0){
+printf("The number is multiple of 3");
+}//end if
+else{
+printf("The number is not multiple of 3");
+}
+return 0;
+}

895c7d85416d8e51c804f38e65414b00071f899d
Hardeep567 <hardeepnaresh8@gmail.com>
2024-09-26 08:40:17 -0700

    Create Question 1.c

---
+++ Lab 5/Question 1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int age; 
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if (age >= 0) {
+        if (age <= 12) {
+            printf("You are a Child.\n");
+        } 
+        else if (age <= 19&& age> 12) {
+            printf("You are a Teenager.\n");
+        }
+        else if (age <= 64 && age > 19) {
+            printf("You are an Adult.\n");
+        }
+        else {
+            printf("You are a Senior.\n");
+        }
+    }
+  else {
+        printf("Invalid age");
+    }
+
+    return 0; 
+}

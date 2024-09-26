947f20ccaff9b68f882ba92d0c77dc44eee668d2
Hardeep567 <hardeepnaresh8@gmail.com>
2024-09-26 09:56:59 -0700

    Create Question 13.c

---
+++ Lab 5/Question 13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char key = 42;  
+    char original_character;
+    char encrypted_value;
+    char decrypted_character;
+    
+    printf("Enter a character to encrypt: ");
+    scanf(" %c", &original_character);  
+
+    encrypted_value = original_character ^ key;
+    printf("Encrypted value: %d\n", encrypted_value);
+
+    decrypted_character = encrypted_value ^ key;
+    printf("Decrypted character: %c\n", decrypted_character);
+
+    return 0;
+}

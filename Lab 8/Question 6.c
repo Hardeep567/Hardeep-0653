8a3cc7773277406593ee2316d63901fd7501d925
Hardeep567 <hardeepnaresh8@gmail.com>
2024-10-27 07:19:05 -0700

    Update Question 6.c

--- Lab 8/Question 6.c
+++ Lab 8/Question 6.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
     int i,j,k,c,arr[3][3];
-    int a[3];
+    int a[3],n[3];
+    int found=0;
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
             scanf("%d", &arr[i][j]);
@@ -17,11 +18,23 @@ int main(){
         a[i]=c;
         c=arr[i+1][0];
     }
-    c=a[0];
     for(i=0;i<3;i++){
-        if(c<a[i]){
-            c=a[i];
+        for(j=0;j<3;j++){
+            if(c<arr[j][i]){
+                c=arr[j][i];
+            }
+        }
+        n[i]=c;
+        c=arr[0][i+1];
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+        if(a[i]==n[j]){
+            printf("%d", a[i]);
+            found=1;
+            n[j]=-99;
+        }
         }
     }
-    printf("Saddle point is = %d", c);
+    if(found==0)printf("No Saddle point");
 }

8203b7b63449e72e57f0bf23b05cb896283c47ed
Hardeep567 <hardeepnaresh8@gmail.com>
2024-11-24 07:48:20 -0800

    Update Question 4.c

--- Lab 10/Question 4.c
+++ Lab 10/Question 4.c
@@ -17,7 +17,7 @@ int main(){
     strcpy(arr[0].genre,"Action");
     strcpy(arr[0].director,"Hardeep Naresh");
     arr[0].releaseyear=2023;
-    arr[0].rating=4.4\n;
+    arr[0].rating=4.4;
    
     int num;
    
@@ -37,18 +37,16 @@ int main(){
     }
     else if(num==2){
        printf("Enter The Name of movie");
-       fgets(arr[i-1].name,30,stdin);
-       getchar();
+       scanf("%s", arr[i-1].name,30,stdin);
+
        printf("Enter The Name of movie genre");
-       fgets(arr[i-1].genre,30,stdin);
-       getchar();
+       scanf("%s",arr[i-1].genre);
+       
        printf("Enter The Name of director");
-       fgets(arr[i-1].director,30,stdin);
-       getchar();
+       scanf("%s", arr[i-1].director);
+       
        printf("Enter the year of release and rating");
-       scanf("%d", &arr[i-1].releaseyear);
-       getchar();
-       scanf("%f", &arr[i-1].rating);
+       scanf("%d  %f", &arr[i-1].releaseyear, &arr[i-1].rating);
        i++;
     }
     }while(num>=1 && num<=2);

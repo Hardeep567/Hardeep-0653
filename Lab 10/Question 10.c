3672ff714f704d03957073b0534c2ee8bfaa7b6d
Hardeep567 <hardeepnaresh8@gmail.com>
2024-11-24 08:53:28 -0800

    Update Question 10.c

--- Lab 10/Question 10.c
+++ Lab 10/Question 10.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-typedef struct package{
+typedef struct package
+{
     char name[30];
     char destination[40];
     int duration;
@@ -9,59 +10,74 @@ typedef struct package{
     int seat;
 } package;
 
-int main(){
+int main()
+{
     package arr[3];
-    strcpy(arr[0].name,"Dubia is far");
-    strcpy(arr[0].destination,"Dubia");
-    arr[0].duration=22;
-    arr[0].cost=3400;
-    arr[0].seat=23;
-    strcpy(arr[1].name,"Islamabad is far");
-    strcpy(arr[1].destination,"Islamabad");
-    arr[1].duration=12;
-    arr[1].cost=340;
-    arr[1].seat=12;
-    strcpy(arr[2].name,"Lahore is far");
-    strcpy(arr[2].destination,"Lahore");
-    arr[2].duration=11;
-    arr[2].cost=400;
-    arr[2].seat=13;
-    int num,i;
-    do{
-        printf("Enter one For Display The Package  and two For Book The Seat in package");
+    strcpy(arr[0].name, "Dubia is not far");
+    strcpy(arr[0].destination, "Dubia");
+    arr[0].duration = 22;
+    arr[0].cost = 3400;
+    arr[0].seat = 23;
+    
+    strcpy(arr[1].name, "Islamabad is side by");
+    strcpy(arr[1].destination, "Islamabad");
+    arr[1].duration = 12;
+    arr[1].cost = 340;
+    arr[1].seat = 12;
+    
+    strcpy(arr[2].name, "Lahore is near");
+    strcpy(arr[2].destination, "Lahore");
+    arr[2].duration = 11;
+    arr[2].cost = 400;
+    arr[2].seat = 13;
+    
+    int num, i;
+    do
+    {
+        printf("Enter : \n1.Display The Package  \n2.Book The Seat in package \n3.Exit\n");
         scanf("%d", &num);
-        if(num==1){
-            printf("%s\n %s\n %d\n %d\n %d\n",arr[0].name,arr[0].destination,arr[0].duration,arr[0].cost,arr[0].seat);
-            printf("%s\n %s\n %d\n %d\n %d\n",arr[1].name,arr[1].destination,arr[1].duration,arr[1].cost,arr[1].seat);
-            printf("%s\n %s\n %d\n %d\n %d\n",arr[2].name,arr[2].destination,arr[2].duration,arr[2].cost,arr[2].seat);
+        if (num == 1)
+        {
+            printf("Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Aavaliable: %d\n\n", arr[0].name, arr[0].destination, arr[0].duration, arr[0].cost, arr[0].seat);
+            printf("Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Aavaliable: %d\n\n", arr[1].name, arr[1].destination, arr[1].duration, arr[1].cost, arr[1].seat);
+            printf("Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Aavaliable: %d\n\n", arr[2].name, arr[2].destination, arr[2].duration, arr[2].cost, arr[2].seat);
         }
-        else if(num==2){
-            printf("Enter the Number of package which you want\n");
-            printf("1. %s\n %s\n %d\n %d\n %d\n",arr[0].name,arr[0].destination,arr[0].duration,arr[0].cost,arr[0].seat);
-            printf("2. %s\n %s\n %d\n %d\n %d\n",arr[1].name,arr[1].destination,arr[1].duration,arr[1].cost,arr[1].seat);
-            printf("3. %s\n %s\n %d\n %d\n %d\n",arr[2].name,arr[2].destination,arr[2].duration,arr[2].cost,arr[2].seat);
-			printf("enter the number");
-			scanf("%d", &i);
+        else if (num == 2)
+        {
+            printf("Enter the Number of package which you want: \n");
+            printf("1. Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Aavaliable: %d\n\n", arr[0].name, arr[0].destination, arr[0].duration, arr[0].cost, arr[0].seat);
+            printf("2. Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Aavaliable: %d\n\n", arr[1].name, arr[1].destination, arr[1].duration, arr[1].cost, arr[1].seat);
+            printf("3. Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Aavaliable: %d\n\n", arr[2].name, arr[2].destination, arr[2].duration, arr[2].cost, arr[2].seat);
+            
+            printf("Enter the number: ");
+            scanf("%d", &i);
             int j;
-            if(i==1){
-                printf("Enter The Number of seats");
+            if (i == 1)
+            {
+                printf("Enter The Number of seats: ");
                 scanf("%d", &j);
-                arr[0].seat-=j;
-                printf("%s\n %s\n %d\n %d\n %d\n",arr[0].name,arr[0].destination,arr[0].duration,arr[0].cost,arr[0].seat);
+                arr[0].seat -= j;
+                printf("Your seats have been booked..\n");
+                printf("Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Remaining: %d\n", arr[0].name, arr[0].destination, arr[0].duration, arr[0].cost, arr[0].seat);
             }
-            else if(i==2){
-                printf("Enter The Number of seats");
+            else if (i == 2)
+            {
+                printf("Enter The Number of seats: ");
                 scanf("%d", &j);
-                arr[1].seat-=j;
-                printf("%s\n %s\n %d\n %d\n %d\n",arr[1].name,arr[1].destination,arr[1].duration,arr[1].cost,arr[1].seat);
+                arr[1].seat -= j;
+                printf("Your seats have been booked..\n");
+                printf("Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Remaining: %d\n", arr[1].name, arr[1].destination, arr[1].duration, arr[1].cost, arr[1].seat);
             }
-            else if(i==3){
-                printf("Enter The Number of seats");
+            else if (i == 3)
+            {
+                printf("Enter The Number of seats: ");
                 scanf("%d", &j);
-                arr[2].seat-=j;
-                printf("%s\n %s\n %d\n %d\n %d\n",arr[2].name,arr[2].destination,arr[2].duration,arr[2].cost,arr[2].seat);
+                arr[2].seat -= j;
+                printf("Your seats have been booked..\n");
+                printf("Package Name: %s\n City: %s\n Travel duration: %d\n Cost: %d\n Seats Remaining: %d\n", arr[2].name, arr[2].destination, arr[2].duration, arr[2].cost, arr[2].seat);
             }
         }
-    }while(num==1 || num==2);
+        printf("\n");
+    } while (num == 1 || num == 2);
     return 0;
 }
